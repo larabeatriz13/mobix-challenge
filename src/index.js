fe8c510e7a16434cfa51d3a7350c5458d3aa1296
Lara Guimaraes <laraguimaraes13@icloud.com>
2021-09-09 23:19:13 -0300

    organization

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const math = require("mathjs")
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+rl.question("digite um calculo matemático: ", (expression)=> 
+{
+    try 
+    {
+    const result =  math.evaluate(expression);
+    console.log(`O valor de ${expression} é ${result}`);
+    rl.close();
+    } 
+    catch (SyntaxError) 
+    {
+    console.log("equação inválida!");
+    rl.close();
+}})
+
